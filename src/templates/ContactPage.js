1c7c4d2d8432044d813f9ca398ac354565cd6b0a
Sean Killeen <SeanKilleen@gmail.com>
2019-06-23 20:35:10 -0400

    remove unnecessary fields from contact form

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { MapPin, Smartphone, Mail } from 'react-feather'
 import { graphql } from 'gatsby'
 
 import PageHeader from '../components/PageHeader'
@@ -29,32 +28,7 @@ export const ContactPageTemplate = ({
       <div className="container Contact--Section1--Container">
         <div>
           <Content source={body} />
-          <div className="Contact--Details">
-            {address && (
-              <a
-                className="Contact--Details--Item"
-                href={`https://www.google.com.au/maps/search/${encodeURI(
-                  address
-                )}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <MapPin /> {address}
-              </a>
-            )}
-            {phone && (
-              <a className="Contact--Details--Item" href={`tel:${phone}`}>
-                <Smartphone /> {phone}
-              </a>
-            )}
-            {email && (
-              <a className="Contact--Details--Item" href={`mailto:${email}`}>
-                <Mail /> {email}
-              </a>
-            )}
-          </div>
         </div>
-
         <div>
           <FormSimpleAjax name="Simple Form Ajax" />
         </div>
